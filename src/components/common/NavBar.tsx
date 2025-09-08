54934a7ff4a1d45007503c8617947437ea1f4d5b
IshiniKiridena <ishini.kiridenaedu@gmail.com>
2025-09-08 19:49:08 +0530

    header UI mobile view

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,6 +1,15 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Menu } from "lucide-react";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
@@ -11,8 +20,8 @@ export default function Navbar() {
           className="sm:hidden"
           src="/assets/logo.png"
           alt="Logo"
-          width={30}
-          height={30}
+          width={50}
+          height={50}
         />
         {/* Desktop Logo */}
         <Image
@@ -25,26 +34,94 @@ export default function Navbar() {
       </Link>
 
       {/* Desktop Links */}
-      <div className="hidden md:flex space-x-6">
-        <button className="hover:underline cursor-pointer font-normal text-base">
+      <div className="hidden md:flex space-x-6 items-center">
+        <Link
+          href="/"
+          className="hover:underline cursor-pointer font-normal text-base"
+        >
           Services
-        </button>
+        </Link>
 
-        <button className="hover:underline cursor-pointer font-normal text-base">
+        <Link
+          href="/"
+          className="hover:underline cursor-pointer font-normal text-base"
+        >
           Testimonials
-        </button>
+        </Link>
 
-        <button className="hover:underline cursor-pointer font-normal text-base">
+        <Link
+          href="/"
+          className="hover:underline cursor-pointer font-normal text-base"
+        >
           About Us
-        </button>
+        </Link>
 
-        <button className="hover:underline cursor-pointer font-normal text-base">
+        <Link
+          href="/"
+          className="hover:underline cursor-pointer font-normal text-base"
+        >
           Contact Us
-        </button>
+        </Link>
 
-        <button className="bg-[#926B48] px-8 py-2 font-semibold text-sm text-white rounded-md cursor-pointer">
+        <Link
+          href="/"
+          className="bg-[#926B48] px-8 py-2 font-semibold text-sm text-white rounded-md cursor-pointer"
+        >
           Book Now
-        </button>
+        </Link>
+      </div>
+
+      {/* Hamburger Menu with Sheet */}
+      <div className="sm:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"}>
+              <Menu className="w-6 h-w-6 text-black" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side="right"
+            className="bg-[#E5E0DA] w-[150px] sm:w-[300px]"
+          >
+            <SheetHeader></SheetHeader>
+            <div className="flex flex-col gap-4 mt-2 items-end pr-4.5">
+              <Link
+                href="/"
+                className="hover:underline cursor-pointer font-normal text-sm"
+              >
+                Services
+              </Link>
+
+              <Link
+                href="/"
+                className="hover:underline cursor-pointer font-normal text-sm"
+              >
+                Testimonials
+              </Link>
+
+              <Link
+                href="/"
+                className="hover:underline cursor-pointer font-normal text-sm"
+              >
+                About Us
+              </Link>
+
+              <Link
+                href="/"
+                className="hover:underline cursor-pointer font-normal text-sm"
+              >
+                Contact Us
+              </Link>
+
+              <Link
+                href="/"
+                className="bg-[#926B48] px-6 py-2 font-semibold text-xs text-white rounded-md cursor-pointer"
+              >
+                Book Now
+              </Link>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
