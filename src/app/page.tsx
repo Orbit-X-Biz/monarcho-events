6e5235b87083cc8690015e5cbc30291c24cf6e9e
IshiniKiridena <ishini.kiridenaedu@gmail.com>
2025-09-23 19:31:54 +0530

    break down the what we do text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,18 +83,27 @@ export default function Home() {
         <h1 className="font-bold text-2xl sm:text-4xl text-[#926B48] mb-4">
           What We Do
         </h1>
-        <p className="font-light text-sm sm:text-xl text-[#926B48]">
-          At Monarcho Events, we transform life’s most cherished moments into
-          luxury experiences that you’ll treasure forever. As a premier event
-          planning and destination management company, we specialize in creating
-          elegant weddings, bespoke proposals, corporate gatherings, and grand
-          cultural celebrations. Our expert planners combine creativity,
-          precision, and a signature touch of royalty to ensure that every
-          detail is flawlessly executed and uniquely reflects your story.
-          Whether you’re dreaming of an intimate celebration or a large-scale
-          event, Monarcho Events brings sophistication, personalization, and
-          unforgettable memories to every occasion.
-        </p>
+        <div className="font-light text-sm sm:text-xl text-[#926B48]">
+          <p>
+            At Monarcho Events, we transform life’s most cherished moments into
+            luxury experiences that you’ll treasure forever.
+          </p>
+          <p>
+            As a premier event planning and destination management company, we
+            specialize in creating elegant weddings, bespoke proposals,
+            corporate gatherings, and grand cultural celebrations.
+          </p>
+          <p>
+            Our expert planners combine creativity, precision, and a signature
+            touch of royalty to ensure that every detail is flawlessly executed
+            and uniquely reflects your story.
+          </p>
+          <p>
+            Whether you’re dreaming of an intimate celebration or a large-scale
+            event, Monarcho Events brings sophistication, personalization, and
+            unforgettable memories to every occasion.
+          </p>
+        </div>
       </div>
 
       {/* Our services component */}
